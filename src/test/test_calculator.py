9a1d7ccb7273c21b6610a2a6a61f321dabbf40ef
tommywenjiezhang <tommyninteenies@gmail.com>
2020-06-24 20:26:08 -0400

    Fix add test message

--- src/test/test_calculator.py
+++ src/test/test_calculator.py
@@ -18,6 +18,7 @@ class TestCalculator(unittest.TestCase):
 
     @parameterized.expand(CsvReader(TEST_DIR + '/Addition.csv').readFile())
     def test_add(self,a,b,expect_result):
+        print( f'{a} + {b} = {expect_result}')
         result = self.calculator.add(int(a) ,int(b))
         self.assertEqual(result, int(expect_result))
     def test_add_error(self):
@@ -27,31 +28,37 @@ class TestCalculator(unittest.TestCase):
 
     @parameterized.expand(CsvReader(TEST_DIR + '/Subtraction.csv').readFile())
     def test_subtract(self, a, b, expected_result):
+        print(f'{a} - {b} = {expected_result}')
         result = self.calculator.minus(int(a), int(b))
         self.assertEqual(result, int(expected_result))
 
     @parameterized.expand(CsvReader(TEST_DIR + '/Division.csv').readFile())
     def test_divide(self, a, b, expected_result):
+        print(f'{b} / {a} = {expected_result}')
         result = self.calculator.divide(float(b), float(a))
         self.assertAlmostEqual(result, float(expected_result))
 
     @parameterized.expand(CsvReader(TEST_DIR + '/Multiplication.csv').readFile())
     def test_multiply(self, a, b, expected_result):
+        print(f'{a} * {b} = {expected_result}')
         result = self.calculator.multiply(int(b), int(a))
         self.assertEqual(result, int(expected_result))
 
     @parameterized.expand(CsvReader(TEST_DIR + '/Square.csv').readFile())
     def test_square(self, a, expected_result):
+        print(f'{a} ^ 2 = {expected_result}')
         result = self.calculator.square(int(a))
         self.assertEqual(result, int(expected_result))
         print(f"Sqare test: {a}^2 ={expected_result}")
 
     @parameterized.expand(CsvReader(TEST_DIR + '/Square Root.csv').readFile())
     def test_sqrt(self,a,expected_result):
+        print(f'{a} ^ -2 = {expected_result}')
         result = self.calculator.sqrt(int(a))
         self.assertAlmostEqual(result,float(expected_result))
 
     def test_sum_list(self):
+        print(f' sumlist [1,2,3] ^ -2 = 6')
         result = self.calculator.sumList([1,2,3])
         self.assertEqual(result,6,"SumList:Test")
 if __name__ == '__main__':
