c7a60194a8ae18e723186b957a57c6269ad35acd
vd265 <60578444+vd265@users.noreply.github.com>
2020-07-06 19:32:26 -0400

    Feature: Random number generator

---
+++ Sampling/SampleCalculator.py
@@ -0,0 +1,11 @@
+#Setup
+import random
+data=[1,2,3,4,5,6,7,8,9,10]
+datalist=[]
+random.seed(a=None,version=None)
+#Population Sampling Functions
+
+#1 - Works
+# print(random.sample(data,,random.randint(1,10)))
+
+#2
