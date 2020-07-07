248076dbfe1ce3240d1ec58fea3a3b24ce0caf6b
vd265 <60578444+vd265@users.noreply.github.com>
2020-07-06 22:15:34 -0400

    task: update random number generator routines again

--- Random/RandomCalculator.py
+++ Random/RandomCalculator.py
@@ -1,52 +1,23 @@
-import random
+class Calculator:
+    def add(self,x,y):
+        if type(x) not in (int, float) :
+            raise ValueError('Value is not int or float')
+        return x+y
+    def minus(self,x,y):
+        return y-x
+    def multiply(self,x,y):
+        return x*y
+    def divide(self,x,y):
+        if y == 0:
+            raise ValueError("Can't be divided by zero")
+        return x/y
+    def sumList(self,lst):
+        result =0
+        for x in range(0, len(lst)):
+            result += lst[x]
+        return result
 
-print("Random integer from 0 to 9")
-num1 = random.randint(0, 9)
-print("Random integer: ", num1)
-
-print("Random integer from 10 to 100")
-num2 = random.randint(10, 100)
-print("Random integer: ", num2)
-
-#Setup
-import random
-data=[1,2,3,4,5,6,7,8,9,10]
-datalist=[]
-random.seed(a=None,version=None)
-
-#Random Generator Functions
-
-# 1 - Generate a random number without a seed between a range of two numbers - Both Integer and decimal
-print(random.randint(0,10))
-
-# 2 - Generate a random number with a seed between a range of two numbers - Both Integer and Decimal
-random.seed(2)
-print(random.randint())
-
-# 3 - Generate a list of numbers with a seed and between a range of numbers - Both Integer and Decimal
-random.seed(1)
-while len(datalist)<random.randint(0,10):
-    datalist.append(random.uniform(0,10))
-    print(datalist)
-
-# 4 - Select a random item from a list
-print(random.choice(data))
-
-# 5 - Set a seed and randomly select the same value from a list
-random.seed(5)
-
-print(random)
-
-#6 - Works?
-# count=0
-# while count<=random.randint(0,10):
-# 	datalist.append(random.choice(data))
-# 	count+=1
-# print(datalist)
-
-#7 - Works?
-# count=0
-# while count<=random.randint(0,10):
-# 	datalist.append(random.choice(data))
-# 	count+=1
-# print(datalist)
+    def square(self,x):
+        return x**2
+    def sqrt(self,x):
+        return math.sqrt(x)
